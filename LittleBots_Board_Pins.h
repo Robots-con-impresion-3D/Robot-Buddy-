90e116aac036acaf9e69828d886a287365c9b78a
AlfonsoCAL <28901517+AlfonsoCalero@users.noreply.github.com>
2023-02-12 22:14:28 +0100

    Delete LittleBots_Board_Pins.h

--- LittleBots_Board_Pins.h
+++
@@ -1,7 +0,0 @@
-//Special Function and REservec Pins are defined in this file
-//You may add other pins for your own expansions
-
-
-#define echoPin A2          // Echo Pin
-#define trigPin A3          // Trigger Pin
-#define buzzerPin 10        // Pin for the buzzer labeled as S9 on the board
